38da23cdf85e6f89df556f92b4ee824a02c4a077
violanerd <madalyne.cross@gmail.com>
2022-07-14 18:11:56 -0500

    added html string

--- app.js
+++ app.js
@@ -1,15 +1,39 @@
 const profileDataArgs = process.argv.slice(2);
+const [namer, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
 
-  console.log('================');
+const generatePage = (userName, githubName) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Portfolio Demo</title>
+    </head>
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+    <body>
+        <h1>${namer}</h1>
+        <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
 };
 
-printProfileData(profileDataArgs);
+console.log(namer, github);
+console.log(generatePage(namer, github));
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
