49a8917f514a5884a58cb77abfb04d5cd3af3899
violanerd <madalyne.cross@gmail.com>
2022-07-15 12:47:37 -0500

    modularize code, write html file

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2);
+
 const [namer, github] = profileDataArgs;
 
+const generatePage = require('./src/page-template');
 
-const generatePage = (userName, githubName) => {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Portfolio Demo</title>
-    </head>
+fs.writeFile('index.html', generatePage(namer, github), err => {
+    if (err) throw new Error(err);
 
-    <body>
-        <h1>${namer}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-
-console.log(namer, github);
-console.log(generatePage(namer, github));
+    console.log('Portfolio complete! CHeck out index.html to see the output!');
+});
 
 // const printProfileData = profileDataArr => {
 //   // This...
